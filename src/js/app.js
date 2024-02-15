cccf9bc4d9ba4fbb05badcd7fe7f393bc6f073eb
Anton Bachurin <ant.bachurin@gmail.com>
2024-02-15 20:43:56 +0400

    не происходит сортировка

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,3 @@
-// TODO: write your code here
 
 export const person = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40};
 
@@ -6,19 +5,29 @@ export const order = ['name', 'level'];
 
 export const orderedPerson = [];
 
-export  function orderByProps(obj) {
-          for (prop in obj) {
-            for (let i = 0; i < order.length; i++) {
-              if (prop === order[i]) {
-                orderedPerson.push(`key: ${prop}, value: ${obj[prop]}`);
-              }
-
-              if (prop !== order[i]) {
-                function compareArguments()
+export function orderByProps(obj) { 
+          const keys = Object.keys(obj);
+          function pushProps(arr) {         
+            for (prop in obj) {
+              for (let i = 0; i < arr.length; i++) {
+                if (prop === arr[i]) {
+                  orderedPerson.push(`key: ${prop}, value: ${obj[prop]}`);
+                }  
               }
             }
           }
+ 
+         pushProps(order);
+ 
+         const result = keys.filter(key => !order.includes(key));
+         const sortedResult = result.sort();
+ 
+         pushProps(sortedResult);
+         return orderedPerson;
+
         }
-  
-orderByProps(person);
-console.log(orderedPerson);
+        
+console.log(orderByProps(person));
+
+
+
